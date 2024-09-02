be9fafe393065f41cdc0da8bb30672da7489996b
marcovalentino-dev <marco.valentino1996@gmail.com>
2024-09-02 19:05:21 +0200

    Update Sound.jsx

--- src/app/components/Sound.jsx
+++ src/app/components/Sound.jsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import { Volume2, VolumeX } from "lucide-react";
 import React, { useEffect, useRef, useState } from "react";
+//
 
 const Sound = () => {
   const audioRef = useRef(null);
